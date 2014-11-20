b17e758af65af0c080c0286ce2396914820b1988
Jerel R Hass <agies1@hotmail.com>
2014-11-20 00:40:28 -0500

    Firsties

---
+++ ProjectLogForms/ProjectLogForms/ShellView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLogForms
+{
+    public partial class ShellView
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
